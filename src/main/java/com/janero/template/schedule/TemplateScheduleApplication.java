bcdc4d29d19b53131069216a5e9b22293e12e855
Eric Janero <eric.janero.ext@idrica.com>
2025-10-13 01:51:05 +0200

    feat: init repo

---
+++ src/main/java/com/janero/template/schedule/TemplateScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.janero.template.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janero/template/schedule/TemplateScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janero.template.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
